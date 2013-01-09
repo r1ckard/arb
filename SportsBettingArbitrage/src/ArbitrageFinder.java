75a5175168f09c1094825049069a86eaae88106c
Rickard Nilsson <rickardnils@gmail.com>
2013-01-09 23:10:52 +0100

    Set up of project

---
+++ SportsBettingArbitrage/src/ArbitrageFinder.java
@@ -0,0 +1,14 @@
+
+public class ArbitrageFinder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Fetch xml from company 1
+		//Fetch xml from company 2
+		
+
+	}
+
+}
---
+++ SportsBettingArbitrage/test/ArbitrageFinderTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ArbitrageFinderTest {
+
+	@Test
+	public void getBettingCompanyXmlLocation() {
+		fail("Not yet implemented");
+	}
+
+}
